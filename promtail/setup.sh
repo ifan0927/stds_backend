51644cbf3fc72cffc185264b2b09d81024b30be7
ifan <ifan@estate.tw>
2025-03-05 00:55:33 +0800

    [FIX]修正promtail問題

--- promtail/setup.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-# 用環境變數替換配置文件中的佔位符
-sed -i "s/LOKI_HOST/${LOKI_HOST}/g" /etc/promtail/promtail-config.yml
-sed -i "s/LOKI_PORT/${LOKI_PORT}/g" /etc/promtail/promtail-config.yml
-
-# 執行 Promtail
-/usr/bin/promtail -config.file=/etc/promtail/promtail-config.yml
